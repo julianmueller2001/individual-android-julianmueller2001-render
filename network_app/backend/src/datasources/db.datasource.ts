621a48dbfd649590dac1f1f7313af3609be30b29
jmueller2001 <566549@student.fontys.nl>
2025-12-19 13:08:08 +0100

    created new db, old one was apparently deleted, fixed migration err

--- network_app/backend/src/datasources/db.datasource.ts
+++ network_app/backend/src/datasources/db.datasource.ts
@@ -4,8 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'postgresloopbacktutorial',
   connector: 'postgresql',
-  url: 'postgresql://julian:3jXTLDJsopBME53jnnSyt93oZOupM6XF@dpg-d30m3gvfte5s73ej4qdg-a.frankfurt-postgres.render.com:5432/postgresloopbacktutorial',
-  ssl: { rejectUnauthorized: false },
+  url: 'postgresql://postgresloopbacktutorial_f3kl_user:Hgyt32i3PBRdNGTwpUwsS07fD3NoqPlg@dpg-d52jniumcj7s73bq0ks0-a.frankfurt-postgres.render.com:5432/postgresloopbacktutorial_f3kl?ssl=true',
 };
 
 // Observe application's life cycle to disconnect the datasource when
--- network_app/backend/src/models/todo.model.ts
+++ network_app/backend/src/models/todo.model.ts
@@ -2,18 +2,7 @@ import {Entity, model, property, belongsTo} from '@loopback/repository';
 import {TodoList, TodoListWithRelations} from './todo-list.model';
 
 
-@model({
-  settings: {
-    foreignKeys: {
-      fk_todo_todoListId: {
-        name: 'fk_todo_todoListId',
-        entity: 'TodoList',
-        entityKey: 'id',
-        foreignKey: 'todoListId',
-      },
-    },
-  },
-})
+@model()
 export class Todo extends Entity {
   @property({
     type: 'number',
