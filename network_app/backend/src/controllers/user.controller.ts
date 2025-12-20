273a15635a9787df72c990a37277225e815adc6d
jmueller2001 <566549@student.fontys.nl>
2025-12-20 14:08:11 +0100

    added missing user controller

---
+++ network_app/backend/src/controllers/user.controller.ts
@@ -0,0 +1,22 @@
+import {inject} from '@loopback/core';
+import {
+  TokenServiceBindings,
+  MyUserService,
+  UserServiceBindings,
+  UserRepository,
+} from '@loopback/authentication-jwt';
+import {TokenService} from '@loopback/authentication';
+import {SecurityBindings, UserProfile} from '@loopback/security';
+import {repository} from '@loopback/repository';
+
+export class UserController {
+  constructor(
+    @inject(TokenServiceBindings.TOKEN_SERVICE)
+    public jwtService: TokenService,
+    @inject(UserServiceBindings.USER_SERVICE)
+    public userService: MyUserService,
+    @inject(SecurityBindings.USER, {optional: true})
+    public user: UserProfile,
+    @repository(UserRepository) protected userRepository: UserRepository,
+  ) {}
+}
